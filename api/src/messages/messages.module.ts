412a6edf98f887a4332b195e4669dbe46bff4d2e
denimorais777 <deni.morais777@gmail.com>
2026-03-16 19:43:40 -0300

    Fix: importar WhatsAppModule em MessagesModule e QueueModul

--- api/src/messages/messages.module.ts
+++ api/src/messages/messages.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { WhatsAppModule } from '../whatsapp/whatsapp.module';
 
 @Module({
+  imports: [WhatsAppModule],
   providers: [MessagesService],
   controllers: [MessagesController],
   exports: [MessagesService],
--- api/src/queue/queue.module.ts
+++ api/src/queue/queue.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { QueueService } from './queue.service';
 import { MessageProcessor } from './message.processor';
+import { WhatsAppModule } from '../whatsapp/whatsapp.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'message-queue',
     }),
+    WhatsAppModule,
   ],
   providers: [QueueService, MessageProcessor],
   exports: [QueueService],
