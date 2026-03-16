7fae54189ac17070c58590e301ae920a6ddf1a99
denimorais777 <deni.morais777@gmail.com>
2026-03-16 19:48:21 -0300

    Fix: importar WhatsAppModule em RulesModule

--- api/src/rules/rules.module.ts
+++ api/src/rules/rules.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RulesService } from './rules.service';
 import { RulesController } from './rules.controller';
+import { WhatsAppModule } from '../whatsapp/whatsapp.module';
 
 @Module({
+  imports: [WhatsAppModule],
   providers: [RulesService],
   controllers: [RulesController],
   exports: [RulesService],
